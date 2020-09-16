1798c1e07a4f96398beffbb8100636fc48b28fff
Alexander Kuziv <makklays@gmail.com>
2020-09-16 14:50:21 +0300

    Create list.php

---
+++ list.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * date: 16.09.2020
+ * author: Alexander.Kuziv
+ * email: alexander@makklays.com.ua
+ */
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>La agenda de contactos en PHP</title>
+    <meta name="description" content="La agenda de contactos en PHP" />
+    <meta name="keywords" content="agenda, contactos, php" />
+    <link rel="canonical" href="http://agenda/" />
+    <meta name="author" content="Makklays" />
+
+    <!-- добавляем скрипты css -->
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <!-- добавляем скрипты js -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <main role="main">
+        <div class="container">
+            <h1>Lista de contactos</h1>
+        </div>
+    </main>
+</body>
+</html>
